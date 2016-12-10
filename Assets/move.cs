62b56977c444945e2b7c8577a0a8c6684cae9b4d
Artur Majewski <artur.majewski2@gmail.com>
2016-12-10 11:26:06 +0100

    Make the walls move towards center.

---
+++ Assets/move.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+    public Vector3 moveVector;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().velocity = moveVector;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
