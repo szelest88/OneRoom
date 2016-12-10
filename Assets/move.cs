1952d7f3103e6b8b83fa2441764bf2833807acc1
Artur Majewski <artur.majewski2@gmail.com>
2016-12-10 12:06:50 +0100

    Stop the walls for a moment.

--- Assets/move.cs
+++ Assets/move.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class move : MonoBehaviour {
 
+    static bool stop = true;
     public Vector3 moveVector;
 	// Use this for initialization
 	void Start () {
+        if(!stop)
         GetComponent<Rigidbody>().velocity = moveVector;
 	}
 	
