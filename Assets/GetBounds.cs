4f1e91205044bc2e21d8f05b217ea5b308cb1313
Artur Majewski <artur.majewski2@gmail.com>
2016-12-11 15:33:16 +0100

    Add some debug info

---
+++ Assets/GetBounds.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetBounds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    public SteamVR_PlayArea playArea;
+    public string info;
+	// Update is called once per frame
+	void Update () {
+        string s =  playArea.vertices.ToString();
+        // vertices - mn x, max x, min z, max z
+
+        info = s;
+    }
+}
