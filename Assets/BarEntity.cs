77e0ee573ac39cdea7401e39a49004aea545d10a
Silnar <m.zdankiewicz@gmail.com>
2016-12-11 17:12:32 +0100

    Add healthbar and shieldbar to orb.

---
+++ Assets/BarEntity.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarEntity : MonoBehaviour
+{
+
+    public GameObject stripe;
+    public float progress = 1.0f;
+
+    private float stripeScaleXInitial;
+
+    void Start()
+    {
+        stripeScaleXInitial = stripe.transform.localScale.x;
+    }
+
+    private void Update()
+    {
+        progress = Mathf.Clamp(progress, 0f, 1f);
+
+        {
+            var stripePosition = stripe.transform.localPosition;
+            stripe.transform.localPosition = new Vector3(
+                -(stripeScaleXInitial - stripeScaleXInitial * progress) * 0.5f,
+                stripePosition.y,
+                stripePosition.z);
+        }
+
+        //{
+        //    var stripeScale = stripe.transform.localScale;
+
+        //    stripe.transform.localScale = new Vector3(
+        //        stripeScaleXInitial * progress,
+        //        stripeScale.y,
+        //        stripeScale.z);
+        //}
+    }
+
+}
