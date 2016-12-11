26509a019bf446fdb654265e304c20233b2feed9
Artur Majewski <artur.majewski2@gmail.com>
2016-12-11 10:12:26 +0100

    Create some crazy enemy logic... Pretty random, but nice IMHO :D

---
+++ Assets/OrbitingBehaviour.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbitingBehaviour : MonoBehaviour {
+    public float radius;
+    public Transform target;
+    public float offset;
+    public bool direction;
+    Rigidbody rigidBody;
+    //Vec
+	// Use this for initialization
+	void Start () {
+        Debug.Log("start orbiting behvior");
+        rigidBody = GetComponent<Rigidbody>();
+        if ((transform.position - target.position).magnitude > radius)
+        {
+            Vector3 velocity =  (target.position - transform.position).normalized;
+            //velocity.y = 0;
+            //rigidBody.velocity = velocity*20;
+         //   rigidBody.AddForce(velocity.normalized * 3,ForceMode.VelocityChange);
+            
+            Debug.Log("setting velocity to " + velocity);
+
+        }
+        else
+        {
+       //     rigidBody.velocity = new Vector3(0, 0, 0);
+
+            Debug.Log("setting velocity to " + rigidBody.velocity);
+        }
+
+	}
+
+    Vector3 calcDir(Vector3 pos, Vector3 target, float radius, float alpha)
+    {
+        Vector3 v = pos - target;
+
+        Vector3 vr = v.normalized * radius;
+
+        Vector3 vrPrim = Matrix4x4.TRS(new Vector3(0, 0, 0), Quaternion.Euler(0, alpha*180.0f/3.1415f, 0), new Vector3(1, 1, 1)).MultiplyPoint3x4(vr);
+        Vector3 targetPoint = target + vrPrim;
+        Vector3 direction = (targetPoint - pos).normalized;
+        return direction;
+
+    }
+    int x = 0;
+    float alpha = 0f;
+    bool velocityNeeds0 = true;
+    // Update is called once per frame
+    void FixedUpdate() {
+        x++;
+        //if (x % 10 == 0)
+        {
+            alpha = Mathf.Atan2(transform.position.x-target.position.z,transform.position.z-target.position.z);        
+            // fix the movement vector
+            rigidBody.AddForce(5*calcDir(transform.position,new Vector3(0,0,0),radius,alpha)); // addforce było nieźle...
+        }
+        if((transform.position - target.position).magnitude <= radius && velocityNeeds0)
+        {
+            rigidBody.velocity = new Vector3(0, 0, 0);
+
+            velocityNeeds0 = false;
+            Debug.Log("setting velocity to " + rigidBody.velocity);
+        }
+
+    }
+}
--- Assets/ProtoEnemyController2.cs
+++ Assets/ProtoEnemyController2.cs
@@ -6,7 +6,6 @@ public class ProtoEnemyController2 : MonoBehaviour {
     Material targetMaterial;
     void Awake()
     {
-        GetComponent<Rigidbody>().AddForce(10, 10, 0);
     }
     IEnumerator colorBack()
     {
@@ -20,13 +19,38 @@ public class ProtoEnemyController2 : MonoBehaviour {
         print(Time.time);
     }
 
+    public void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+
+        }
+    }
+    void dieBastard()
+    {
+
+    }
     private void OnTriggerEnter(Collider other)
     {
        // transform.position = new Vector3(transform.position.x, 3-transform.position.y, transform.position.z);
-        Material targetMaterial = GetComponent<MeshRenderer>().material;
-        targetMaterial.SetColor("_Color",Color.green);
-
-        StartCoroutine(colorBack());
+        //Material targetMaterial = GetComponent<MeshRenderer>().material;
+        //Color c = targetMaterial.color;
+        
+        //c.b -= 0.1f;
+        //c.r -= 0.1f;
+        //c.g -= 0.1f;
+        //c.a -= 0.25f;
+        //targetMaterial.color = c;
+        foreach (MeshRenderer m in GetComponentsInChildren<MeshRenderer>())
+        {
+            Material targetMaterial = m.material;
+            Color c = targetMaterial.color;
+            c.a -= 1f;
+            targetMaterial.color = c;
+            m.material = targetMaterial;
+        }
+        GetComponent<SphereCollider>().radius = 0.28f;
+        //StartCoroutine(colorBack());
         other.gameObject.GetComponent<MeshRenderer>().enabled = false;
     }
 }
